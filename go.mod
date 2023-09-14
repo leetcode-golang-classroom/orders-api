412149fc6cf2dfa5239b65c4094baba888f08dac
gson Liang <yuanyu90221@gmail.com>
2023-09-15 01:37:15 +0800

    feat(add model , repository): add model , repository
    add model , repository

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/google/uuid v1.3.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
---
+++ repository/order/redis.go
@@ -0,0 +1,136 @@
+package order
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/leetcode-golang-classroom/orders-api/models"
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisRepo struct {
+	Client *redis.Client
+}
+
+func orderIdKey(id uint64) string {
+	return fmt.Sprintf("order:%d", id)
+}
+func (r *RedisRepo) Insert(ctx context.Context, order models.Order) error {
+	data, err := json.Marshal(order)
+	if err != nil {
+		return fmt.Errorf("failed to encode order: %w", err)
+	}
+	key := orderIdKey(order.OrderId)
+	txn := r.Client.TxPipeline()
+	res := txn.SetNX(ctx, key, string(data), 0)
+	if err := res.Err(); err != nil {
+		txn.Discard()
+		return fmt.Errorf("failed to set: %w", err)
+	}
+	if err := txn.SAdd(ctx, "orders", key).Err(); err != nil {
+		txn.Discard()
+		return fmt.Errorf("failed to add to orders set: %w", err)
+	}
+	if _, err := txn.Exec(ctx); err != nil {
+		return fmt.Errorf("failed to exec: %w", err)
+	}
+	return nil
+}
+
+var ErrNotExists = errors.New("order does not exist")
+
+func (r *RedisRepo) FindById(ctx context.Context, id uint64) (models.Order, error) {
+	key := orderIdKey(id)
+
+	value, err := r.Client.Get(ctx, key).Result()
+	if errors.Is(err, redis.Nil) {
+		return models.Order{}, ErrNotExists
+	} else if err != nil {
+		return models.Order{}, fmt.Errorf("get order: %w", err)
+	}
+	var order models.Order
+	err = json.Unmarshal([]byte(value), &order)
+	if err != nil {
+		return models.Order{}, fmt.Errorf("failed to decode order json:%w", err)
+	}
+	return order, nil
+}
+
+func (r *RedisRepo) DeleteById(ctx context.Context, id uint64) error {
+	key := orderIdKey(id)
+	txn := r.Client.TxPipeline()
+	err := txn.Del(ctx, key).Err()
+	if errors.Is(err, redis.Nil) {
+		txn.Discard()
+		return ErrNotExists
+	} else if err != nil {
+		txn.Discard()
+		return fmt.Errorf("get order: %w", err)
+	}
+	if err := txn.SRem(ctx, "orders", key).Err(); err != nil {
+		txn.Discard()
+		return fmt.Errorf("failed to remove from orders set: %w", err)
+	}
+	if _, err := txn.Exec(ctx); err != nil {
+		return fmt.Errorf("failed to exec: %w", err)
+	}
+	return nil
+}
+
+func (r *RedisRepo) Update(ctx context.Context, order models.Order) error {
+	data, err := json.Marshal(order)
+	if err != nil {
+		return fmt.Errorf("failed to encode order: %w", err)
+	}
+	key := orderIdKey(order.OrderId)
+	err = r.Client.SetNX(ctx, key, string(data), 0).Err()
+	if errors.Is(err, redis.Nil) {
+		return ErrNotExists
+	} else if err != nil {
+		return fmt.Errorf("set order: %w", err)
+	}
+	return nil
+}
+
+type FindAllPage struct {
+	Size   uint
+	Offset uint
+}
+type FindResult struct {
+	Orders []models.Order
+	Cursor uint64
+}
+
+func (r *RedisRepo) FindAll(ctx context.Context, page FindAllPage) (FindResult, error) {
+	res := r.Client.SScan(ctx, "orders", uint64(page.Offset), "*", int64(page.Size))
+	keys, cursor, err := res.Result()
+	if err != nil {
+		return FindResult{}, fmt.Errorf("failed to get order ids: %w", err)
+	}
+	if len(keys) == 0 {
+		return FindResult{
+			Orders: []models.Order{},
+		}, nil
+	}
+	xs, err := r.Client.MGet(ctx, keys...).Result()
+	if err != nil {
+		return FindResult{}, fmt.Errorf("failed to get orders: %w", err)
+	}
+	orders := make([]models.Order, len(xs))
+
+	for i, x := range xs {
+		x := x.(string)
+		var order models.Order
+		err := json.Unmarshal([]byte(x), &order)
+		if err != nil {
+			return FindResult{}, fmt.Errorf("failed to decode order json: %w", err)
+		}
+		orders[i] = order
+	}
+	return FindResult{
+		Orders: orders,
+		Cursor: cursor,
+	}, nil
+}
